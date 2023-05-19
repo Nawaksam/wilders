1ffd1451a73bbab0ad9dd7f42dd5e29f221b8b3f
Nawaksam <sam.cardonnel@gmail.com>
2023-05-19 12:48:34 +0200

    -created

---
+++ index.js
@@ -0,0 +1,9 @@
+import chalk from "chalk";
+
+console.log(chalk.hex("#ff0000")(`Adrien`));
+console.log(chalk.hex("#ffa500")(`Annie`));
+console.log(chalk.hex("#ffff00")(`Anaïs`));
+console.log(chalk.hex("#008000")(`Cédric`));
+console.log(chalk.hex("#0000ff")(`Nicolas`));
+console.log(chalk.hex("#4b0082")(`Sam`));
+console.log(chalk.hex("#ee82ee")(`Tristan`));
